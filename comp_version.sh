bc0831eec08e856e395cabca4bd7a89eff870b65
Alexander Naumov <alexander_naumov@opensuse.org>
2019-07-17 16:27:53 +0200

    adding DIFF support

--- comp_version.sh
+++ comp_version.sh
@@ -2,14 +2,15 @@
 
 pprintf() {
     if [ -z $3 ]; then
-        printf "%40s%15s%15s%15s\n" $1 $2 "-----" "?????"
+        printf "%30s%12s%12s    %s\n" $1 $2 "-----" "?????"
         return	
     fi
 
     if [ ! "$3" == "$2" ]; then
-        printf "%40s%15s%15s%15s\n" $1 $2 $3 "SYNC NEEDED!"
+        DIFF="https://cvsweb.openbsd.org/cgi-bin/cvsweb/www/$1.diff?r1=$2&r2=$3"
+        printf "%30s%12s%12s    %s\n" $1 $2 $3 $DIFF
     else
-        printf "%40s%15s%15s%15s\n" $1 $2 $3 "OK" 
+        printf "%30s%12s%12s    %s\n" $1 $2 $3 "OK"
     fi
 }
 
@@ -31,10 +32,10 @@ else
     home=$1
 fi
 
-printf "%40s%15s%15s%15s\n" "FILE" "LOCAL" "UPSTREAM" "STATE"
+printf "%30s%12s%12s    %s\n" "FILE" "LOCAL" "UPSTREAM" "STATE"
 for file in $(html_walk $home); do 
     WEB=$(curl -is https://cvsweb.openbsd.org/cgi-bin/cvsweb/www/$file?only_with_tag=HEAD);
     WEB=$(echo "$WEB"|grep 'Revision'|sed -e 's/<[^>]*>//g'|awk '{print $2}');
     GIT=$(git log --oneline $file | cut -d ' ' -f 2 | sed 's/[][]//g' | head -1)
-    pprintf $file $GIT $WEB
+    pprintf ${file:2} $GIT $WEB
 done
